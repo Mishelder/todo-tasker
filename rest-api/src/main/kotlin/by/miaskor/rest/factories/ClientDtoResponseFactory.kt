fd0849272c1cf4e41de68510a608bb74a3589c37
m.skorohodv <mikhail.skorokhodov@moneyman.ru>
2021-11-11 23:33:43 +0300

    Create dto's factories for rest-api service

---
+++ rest-api/src/main/kotlin/by/miaskor/rest/factories/ClientDtoResponseFactory.kt
@@ -0,0 +1,18 @@
+package by.miaskor.rest.factories
+
+import by.miaskor.rest.api.dto.ClientDtoResponse
+import by.miaskor.rest.store.entities.ClientEntity
+import org.springframework.stereotype.Component
+
+@Component
+class ClientDtoResponseFactory {
+
+  fun makeClientDtoResponse(clientEntity: ClientEntity): ClientDtoResponse {
+    return ClientDtoResponse(
+      id = clientEntity.id,
+      email = clientEntity.email,
+      login = clientEntity.login,
+      password = clientEntity.password
+    )
+  }
+}
