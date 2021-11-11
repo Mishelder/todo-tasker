c0822012689b6b929132f8edae524542733b5e31
m.skorohodv <mikhail.skorokhodov@moneyman.ru>
2021-11-11 23:35:12 +0300

    Add app

---
+++ rest-api/src/main/kotlin/by/miaskor/rest/Application.kt
@@ -0,0 +1,13 @@
+package by.miaskor.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+  runApplication<Application>(*args)
+}
+
+
