f70b009e8452ddcc086f91498f80025fbdb7e849
m.skorohodv <mikhail.skorokhodov@moneyman.ru>
2021-11-26 01:16:38 +0300

    move here templates

--- webapp/src/main/java/com/miaskor/todo/spring/controller/ViewController.java
+++ webapp/src/main/java/com/miaskor/todo/spring/controller/ViewController.java
@@ -1,10 +1,19 @@
 package com.miaskor.todo.spring.controller;
 
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+
+  private final HttpSession httpSession;
+
+  @Autowired
+  public ViewController(HttpSession httpSession) {
+    this.httpSession = httpSession;
+  }
 
   @GetMapping("/todo")
   public String getToDo() {
@@ -14,5 +23,21 @@ public class ViewController {
   @GetMapping("/about")
   public String getAbout() {
     return "about";
+  }
+
+  @GetMapping("/inbox")
+  public String getInbox() {
+    return "inbox";
+  }
+
+  @GetMapping("/logout")
+  public String loggedOut() {
+    httpSession.invalidate();
+    return "authorization";
+  }
+
+  @GetMapping("/auth")
+  public String getLoginPage() {
+    return "authorization";
   }
 }
